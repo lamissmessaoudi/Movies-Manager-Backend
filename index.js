6d27275fc2b9fc97844b6ad36570708d05df271e
Lamiss Messaoudi <younglamiss@gmail.com>
2021-03-20 00:37:38 +0100

    HTTP GET Requests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-var _ = require('underscore')
+const express = require('express'); //express is a function
+const app = express();
+
+const courses = [
+    { id: 1, name: 'course1 ' },
+    { id: 2, name: 'course2 ' },
+    { id: 3, name: 'course3 ' }
+]
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send('NOT FOUND ')//404
+    res.send(course)
+})
+
+const port = process.env.PORT || 3000;
+// to change PORT:  set PORT=5000 in cmd
+app.listen(port, () => {
+    console.log(port);
+});
