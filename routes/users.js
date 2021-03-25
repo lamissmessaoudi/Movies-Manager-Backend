311be061f1960a4ca70984121ea81d209992265f
Lamiss Messaoudi <younglamiss@gmail.com>
2021-03-25 17:00:10 +0100

    modify register response with lodash

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const _ = require('lodash')
 const mongoose = require('mongoose')
 const { validateuser, User } = require('../models/user')
 
@@ -15,14 +15,12 @@ router.post('/', async (req, res) => {
 
     let user = await User.findOne({ email: req.body.email })
     if (user) return res.status(400).send("User already registered")
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password // we can read it thnx to the middleware
-    })
+    user = new User(_.pick(req.body, ['name', 'email', 'password']))
     await user.save();
 
-    res.send(user);
+
+
+    res.send(_.pick(user, ['_id', 'name', 'email']))
 })
 
 
